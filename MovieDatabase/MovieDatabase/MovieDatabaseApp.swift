515e67f6ced28beefdb8e5b161e9898436624644
Nilaakash Singh <singhnilaakash@gmail.com>
2021-09-24 13:49:31 +0530

    Initial commit

---
+++ MovieDatabase/MovieDatabase/MovieDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDatabaseApp.swift
+//  MovieDatabase
+//
+//  Created by Nilaakash Singh on 24/09/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
